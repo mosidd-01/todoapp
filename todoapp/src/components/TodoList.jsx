d1b80532e8387b150e8439ba1f4ef5ebb7a7131a
Muhammad Siddiqui <s760132@gmail.com>
2024-04-21 03:30:12 -0500

    added sorting to todo list

--- todoapp/src/components/TodoList.jsx
+++ todoapp/src/components/TodoList.jsx
@@ -1,9 +1,12 @@
 import TodoItem from "./TodoItem";
 
 export default function TodoList({ todos, setTodos }) {
+  const sortedTodos = todos
+    .slice()
+    .sort((a, b) => Number(a.done) - Number(b.done));
   return (
     <div className="bg-gray-700 rounded-lg shadow-md w-1/3 m-auto px-5 py-5 mt-12">
-      {todos.map((item) => (
+      {sortedTodos.map((item) => (
         <TodoItem
           key={item.name}
           item={item}
